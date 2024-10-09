4cbde8be1c4d8b14e032092a556c79591b7af2f1
Patricia9707 <serakalalapatricia@gmail.com>
2024-10-09 15:06:31 +0200

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,14 +1,9 @@
 function updateCity(event) {
   let cityTimeZone = event.target.value;
-  if (cityTimeZone === "current") 
-  {
-    cityTimeZone = moment.tz.guess();
-  }
-  // Get the city's name and time
+  if (cityTimeZone === "") return; // Prevent errors if no city is selected
+
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
-
-  // Dynamically create the city block with the time and date for the selected city
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
         <div class="city">
@@ -26,30 +21,35 @@ function updateCity(event) {
 
 // Initialize the clock for existing cities
 function updateTime() {
+  // Johannesburg
+  let johannesburgElement = document.querySelector("#johannesburg");
+  let johannesburgTime = moment().tz("Africa/Johannesburg");
+  johannesburgElement.querySelector(".date").innerHTML =
+    johannesburgTime.format("MMMM Do YYYY");
+  johannesburgElement.querySelector(".time").innerHTML =
+    johannesburgTime.format("h:mm:ss A");
+
   // Los Angeles
   let losAngelesElement = document.querySelector("#los-angeles");
   let losAngelesTime = moment().tz("America/Los_Angeles");
   losAngelesElement.querySelector(".date").innerHTML =
     losAngelesTime.format("MMMM Do YYYY");
-  losAngelesElement.querySelector(".time").innerHTML = losAngelesTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+  losAngelesElement.querySelector(".time").innerHTML =
+    losAngelesTime.format("h:mm:ss A");
 
   // Tokyo
   let tokyoElement = document.querySelector("#tokyo");
   let tokyoTime = moment().tz("Asia/Tokyo");
   tokyoElement.querySelector(".date").innerHTML =
     tokyoTime.format("MMMM Do YYYY");
-  tokyoElement.querySelector(".time").innerHTML = tokyoTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+  tokyoElement.querySelector(".time").innerHTML = tokyoTime.format("h:mm:ss A");
 
   // Paris
   let parisElement = document.querySelector("#paris");
   let parisTime = moment().tz("Europe/Paris");
   parisElement.querySelector(".date").innerHTML =
     parisTime.format("MMMM Do YYYY");
-  parisElement.querySelector(".time").innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+  parisElement.querySelector(".time").innerHTML = parisTime.format("h:mm:ss A");
 }
 
 // Update time every second for the static cities
