c7f48db5bafb046dc654852c96572a4b43b8b1db
Patricia9707 <serakalalapatricia@gmail.com>
2024-10-09 12:58:20 +0200

    made changes in the html and javascript

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,43 @@
-alert("Hello World!")
+function updateTime() {
+  // Johannesburg
+  let johannesburgElement = document.querySelector("#johannesburg");
+  let johannesburgDateElement = johannesburgElement.querySelector(".date");
+  let johannesburgTimeElement = johannesburgElement.querySelector(".time");
+  let johannesburgTime = moment().tz("Africa/Johannesburg");
+
+  johannesburgDateElement.innerHTML = johannesburgTime.format("MMMM Do YYYY");
+  johannesburgTimeElement.innerHTML = johannesburgTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  // Los Angeles
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  // Tokyo
+  let tokyoElement = document.querySelector("#tokyo");
+  let tokyoDateElement = tokyoElement.querySelector(".date");
+  let tokyoTimeElement = tokyoElement.querySelector(".time");
+  let tokyoTime = moment().tz("Asia/Tokyo");
+
+  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
+
+  // Paris
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+
+  parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+  parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
+}
+updateTime();
+setInterval(updateTime,1000) 
